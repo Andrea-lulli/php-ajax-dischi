01f89710d7cc702d3da47a742dd6c3da44647ad9
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-12-15 16:53:54 +0100

    esercizio completo

--- api/index.php
+++ api/index.php
@@ -3,8 +3,28 @@ include __DIR__ . '/../database/index.php';
 
 // var_dump( __DIR__ );
 
+$arrayGenereFiltrato = [];
+
+
+if( !empty($_GET) && !empty($_GET['genre'] ) ){
+
+  foreach( $database as $elem ){
+    
+    if( $elem['genre'] == $_GET['genre'] ){
+     
+      $arrayGenereFiltrato[] = $elem;
+    }
+  }
+
+
+} else {
+
+  $arrayGenereFiltrato = $database;
+}
+
+
 header('Content-type: application/json');
 
-echo json_encode($database);
+echo json_encode($arrayGenereFiltrato);
 ?>
 
--- js/main.js
+++ js/main.js
@@ -2,19 +2,36 @@ var app = new Vue({
   el: '#app',
   data: {
     arrayMusica: [],
+    arrayGenere: [],
+    scelta: '',
 
   },
   mounted() {
     axios.get('http://localhost/php-ajax-dischi/api')
       .then((resp) => {
         this.arrayMusica = resp.data
-        
+
+        this.arrayMusica.forEach((elem) => {
+          console.log(elem);
+          if (!this.arrayGenere.includes(elem.genre)) {
+            this.arrayGenere.push(elem.genre);
+          }
+
+        })
+
       })
   },
   beforeUpdate() {
 
   },
   methods: {
+    sceltaGenere() {
+      axios.get(`http://localhost/php-ajax-dischi/api/?genre=${this.scelta}`)
+        .then((resp) => {
+          this.arrayMusica = resp.data
+        })
 
+
+    }
   }
 })
